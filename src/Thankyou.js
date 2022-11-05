e2cfd1f090b758608912d3abb115fa40ae96b44e
ibimina <96787210+ibimina@users.noreply.github.com>
2022-11-05 21:27:53 +0100

    docs: styled thank you page

--- src/Thankyou.js
+++ src/Thankyou.js
@@ -1,18 +1,19 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 export default function Thankyou() {
-    const nav = useNavigate()
+  const nav = useNavigate();
   const handleSubmit = (e) => {
     e.preventDefault();
- 
-nav("/")
 
+    nav("/");
   };
   return (
     <div className="thanks">
-      <h1 className="thankyou">Thanks for the message!</h1>
-<p>I'll get back to you as soon as possible</p>
-      <button onClick={handleSubmit} className="home">BACK TO PORTFOLIO</button>
+      <h2 className="thankyou">Thanks for the message!</h2>
+      <p className="res">I'll get back to you as soon as possible</p>
+      <button onClick={handleSubmit} className="home">
+        BACK TO PORTFOLIO
+      </button>
     </div>
   );
 }
