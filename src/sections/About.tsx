1e41b8f91b06178cedd462aec8354288f84c9f2f
Ibimina Hart <ibimina.c.hart@gmail.com>
2024-05-10 22:55:12 +0100

    Update About.tsx
    Upadate about me section

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -3,7 +3,7 @@ export default function About() {
   return (
     <section id="about" className={styles.about}>
       <p className={styles.margin}>Hi there!
-       I'm a frontend engineer with experience in web development. I'm passionate about creating visually amazing web experiences and bringing creative concepts to life with code.
+       I'm a software Engineer specialize in developing visually stunning web applications and transforming creative visions into interactive digital experiences.
       </p>
       <p>I'm most likely to be found playing card games or watching movies when I'm not coding. So please contact me if you have any movie recommendations!</p>
     </section>
